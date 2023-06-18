157fddc6f9650fdeab9f496fd1eecb8242291b0b
Juyeon Lee <lqu134@naver.com>
2023-06-19 00:25:17 +0900

    js - destructuring

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,24 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+//Destructuring
+
+const book = getBook(2)
+book;
+
+// const title = book.title;
+// const author = book.author
+
+const {title, author, pages, publicationDate, genres, hasMovieAdaptation} = book;
+
+title;
+author;
+console.log(author,title,genres);
+
+// const primaryGenre = genres[0]
+// const secondaryGenre = genres[1]
+
+const [primaryGenre,secondaryGenre] = genres
+
+console.log(primaryGenre, secondaryGenre)
