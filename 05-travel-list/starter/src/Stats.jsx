ec6507474988e1a459aaf9fae06555f83d8431a9
Juyeon Lee <lqu134@naver.com>
2023-07-08 23:39:10 +0900

    used derived state

--- 05-travel-list/starter/src/Stats.jsx
+++ 05-travel-list/starter/src/Stats.jsx
@@ -1,12 +1,22 @@
 export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding soe items to your packing list</em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className="stats">
       <em>
-        🤳You have {numItems} items on your list, and you already packed{" "}
-        {numPacked}(X%)
+        {percentage === 100
+          ? "You got everything! Ready to go!"
+          : ` 🤳You have ${numItems} items on your list, and you already packed
+        ${numPacked}(${percentage}%)`}
       </em>
     </footer>
   );
