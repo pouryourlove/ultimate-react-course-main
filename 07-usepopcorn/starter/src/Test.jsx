d4d04c0993f7ea396af0d32f06ec41936af83007
pouryourlove <lqu134@naver.com>
2023-07-25 20:07:23 +0900

    improved reusability

---
+++ 07-usepopcorn/starter/src/Test.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import StarRating from "./StarRating";
+
+export default function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
--- 07-usepopcorn/starter/src/main.jsx
+++ 07-usepopcorn/starter/src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import Test from "./Test";
 // import App from './App.jsx'
 // import './index.css'
-
-import StarRating from "./StarRating";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
