b57ddb0d8224fb98373cffc029e9574f16f226c7
Juyeon Lee <lqu134@naver.com>
2023-12-24 22:59:24 +0900

    finishing the quiz

--- 10-react-quiz/starter/src/components/App.jsx
+++ 10-react-quiz/starter/src/components/App.jsx
@@ -5,6 +5,9 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StarScreen from "./StarScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
     //'loading','error','ready','active','finished'
     status: "loading",
     index: 0,
+    answer: null,
+    points: 0,
+    highscore:0
   };
   function reducer(state, action) {
     switch (action.type) {
@@ -31,16 +37,33 @@ function App() {
       case "start":
         return {
           ...state, 
-          status: "active"}  
+          status: "active"}
+      case "newAnswer":
+          const question = state.questions.at(state.index);
+
+        return {
+          ...state, 
+          answer: action.payload,
+          points: action.payload === question.correctOption ? state.points + question.points : state.points
+        }    
+      case "nextQuestion":
+        return {...state, index: state.index + 1, answer: null}  
+      
+      case "finish":
+      return {...state, 
+        status: "finished",
+        highscore: 
+              state.points > state.highscore ? state.points : state.highscore}
 
       default:
         throw new Error("Action unknown");
     }
   }
 
-  const [{questions,status,index}, dispatch] = useReducer(reducer, initialState);
+  const [{questions,status,index,answer, points,highscore}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length
+  const maxPossiblePoints = questions.reduce((prev,cur) => prev + cur.points,0)
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -55,7 +78,14 @@ function App() {
       <Text>{status === 'loading'&&<Loader/>}
       {status === 'error'&&<Error/>}
       {status === 'ready'&&<StarScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-      {status === "active" && <Question question={questions[index]}/>}
+      {status === "active" && 
+      <>
+      <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints = {maxPossiblePoints} answer={answer}/>
+      <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
+      <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+      </>
+      }
+      {status === 'finished' && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore}/>}
       </Text>
     </div>
   );
