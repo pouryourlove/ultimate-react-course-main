2aa6a261d504871d3f27f04540f51b5e5902580e
pouryourlove <lqu134@naver.com>
2023-12-30 18:24:48 +0900

    css module

--- 11-worldwise/starter/src/components/PageNav.jsx
+++ 11-worldwise/starter/src/components/PageNav.jsx
@@ -1,22 +1,22 @@
-import {  NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/pricing">Pricing</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/product">Product</NavLink>
-                </li>
-            </ul>
-            
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default PageNav
+export default PageNav;
