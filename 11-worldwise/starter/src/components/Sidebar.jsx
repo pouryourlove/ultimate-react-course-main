d6cef57487d23e964cfa02f2c5e8691795a67637
Juyeon Lee <lqu134@naver.com>
2024-01-02 01:57:43 +0900

    Building AppLayout page

---
+++ 11-worldwise/starter/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import AppNav from './AppNav'
+import Logo from './Logo'
+import styles from './Sidebar.module.css'
+
+function Sidebar() {
+    return (
+        <div className={styles.sidebar}>
+            <Logo/>
+            <AppNav/>
+            <p>List of cities</p>
+            <footer className={styles.footer}></footer>
+            <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()}by WorldWise Inc.</p>
+            
+        </div>
+    )
+}
+
+export default Sidebar
