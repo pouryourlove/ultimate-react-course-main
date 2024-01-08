35ba646fbfdf6d35031445877d7c307fa5d29498
Juyeon Lee <lqu134@naver.com>
2024-01-08 17:51:59 +0900

    added countries lists

--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountriesList from "./components/CountriesList";
 
 
 const BASE_URL = "http://localhost:8000";
@@ -40,7 +41,7 @@ function App() {
         <Route path="app" element={<AppLayout />}> 
           <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
-          <Route path="countries" element={<p>countries</p>}/>
+          <Route path="countries" element={<CountriesList cities={cities} isLoading={isLoading}/>}/>
           <Route path="form" element={<p>form</p>}/>
 
         </Route>
